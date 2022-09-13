3b74bb71eceb28b115bc91d5486567d59963b0f1
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-13 16:25:56 +0900

    エラーハンドリング実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
 
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
          validates :birth_day, presence: true
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with:  VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'
 end
