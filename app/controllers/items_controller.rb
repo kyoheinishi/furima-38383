7bc563f3c3529496384d105728885f7b29162499
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-15 14:29:57 +0900

    itemコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,10 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
-      @item = @item
       render :new
     end
   end
