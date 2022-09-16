b44d3a05f230867d000ddad8478fde00b7dd6f1c
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-16 14:38:19 +0900

    商品削除機能実装route,cotroller作成、表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
-  before_action :item_set,  only: [:show, :edit, :update]
+  before_action :item_set,  only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
