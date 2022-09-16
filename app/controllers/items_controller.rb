f7678e6b83617954fccd97e4e363191cb3218d25
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-16 14:07:35 +0900

    変数代入のbefore_action作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :item_set,  only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -47,4 +46,10 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     redirect_to action: :index if item.user_id != current_user.id
   end
+
+  def item_set
+    @item = Item.find(params[:id])
+  end
+
+
 end
