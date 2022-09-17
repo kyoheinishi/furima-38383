8b6ed50ebcb0a96a79d66fe0dc631980f5d22112
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-17 19:13:32 +0900

    複数モデルにデータ保存、エラーコード表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,26 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
+    @order_purchaser = OrderPurchaser.new
+  end
+
+  def create
+    @order_purchaser = OrderPurchaser.new(order_purchaser_params)
+    @item = Item.find(params[:item_id])
+    if @order_purchaser.valid?
+       @order_purchaser.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_purchaser_params
+    params.require(:order_purchaser).permit(:post_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
