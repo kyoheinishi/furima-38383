b7e5dd0c2f728c62ca824f8cd86019d258888058
kyoheinishi <kyohei.nishi@gmail.com>
2022-09-18 16:17:57 +0900

    コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :item_id_set, only: [:index, :create]
-  before_action :move_to_index, only: [:edit, :index]
+  before_action :move_to_index, only: [:index]
 
   def index
     if @item.order.present?
